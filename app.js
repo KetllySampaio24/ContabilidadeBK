3a8a33b56bebdef513938ba0dcdb14302859b8f7
Kétlly Sampaio <ketlly.sampaio17@gmail.com>
2025-07-21 12:41:49 -0300

    Commit referente a inclusão do projeto no github

---
+++ app.js
@@ -0,0 +1,42 @@
+
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const nome = form.name.value;
+    const email = form.email.value;
+    const mensagem = form.message.value;
+
+    fetch("https://formsubmit.co/ajax/assessoriacontabilbk@gmail.com", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: nome,
+            email: email,
+            message: mensagem,
+            _subject: `Mensagem de ${nome} via site Contabilidade BK`,
+            _template: "table",
+            _captcha: "false"
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("✅ Sua mensagem foi enviada com sucesso!");
+            form.reset();
+        } else {
+            alert("❌ Ocorreu um erro ao enviar sua mensagem.");
+        }
+    })
+    .catch(error => {
+        alert("⚠️ Erro de conexão. Tente novamente.");
+    });
+});
+
+window.addEventListener("scroll", function () {
+  const scrollBtn = document.getElementById("scrollTopBtn");
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
